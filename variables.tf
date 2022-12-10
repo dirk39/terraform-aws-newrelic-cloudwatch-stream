a22ec0f06b7f4be855e5b60af4dbed488dedf81c
Andrea Quintino <andreaquin1990@gmail.com>
2022-12-10 11:36:50 +0100

    setup IAM role variables

--- variables.tf
+++ variables.tf
@@ -1,21 +1,39 @@
-variable "tags" {
+variable "create_newrelic_iam" {
+  type        = bool
+  description = "Controls if NewRelic IAM should be created"
+  default     = true
+}
+
+variable "newrelic_iam_role_name" {
+  type        = string
+  description = "NewRelic IAM role name"
+  default     = "NewRelicMonitoringRole"
+}
+
+variable "newrelic_iam_role_description" {
+  type        = string
+  description = "NewRelic IAM role description"
+  default     = "Role used by NewRelic infrastructure to monitor the account"
+}
+
+variable "newrelic_iam_role_tags" {
   type        = map(any)
-  description = "Default tags to use"
+  description = "NewRelic IAM role tags"
   default     = {}
 }
 
-variable "environment" {
+variable "newrelic_iam_role_aws_account_id" {
   type        = string
-  description = "Environment name"
+  description = "NewRelic AWS account id to trust"
+  default     = "754728514883"
 }
 
-variable "s3_bucket_module_version" {
-  type        = string
-  description = "Default tags to use"
-}
-
-variable "newrelic_license_key" {
+variable "newrelic_iam_role_license_key" {
   type        = string
   description = "NewRelic license key"
-  sensitive   = true
+}
+
+variable "newrelic_iam_enable_budget_monitoring" {
+  type        = bool
+  description = "Enable budget cost monitoring"
 }
